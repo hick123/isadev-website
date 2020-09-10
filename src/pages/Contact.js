5de37d5808b1d83bdaffc4ded2f2cd6511ba47a8
isaiah korir <korir.isaiah@gmail.com>
2020-09-11 00:52:58 +0300

    add work samples

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
+import twitter from "../assets/img/logo-twitter.svg";
+import phone from "../assets/img/phone-handset-line.svg";
+import email from "../assets/img/email (1).svg";
 
 function Contact() {
   return (
@@ -9,6 +12,39 @@ function Contact() {
         span="Touch"
         p="Will get back to you as soon as possible"
       />
+      <div className="contact-details">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="contact-details-content">
+              <img src={phone} alt="" />
+              <h4>Call Me</h4>
+              <p>+254 718445568</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="contact-details-content">
+              <img src={twitter} alt="" />
+              <h4>Twitter</h4>
+              <p>@ikorir</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="contact-details-content">
+              <img src={email} alt="" />
+              <h4>Email Me</h4>
+              <p>korir.isaiah@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="contact-text">
+        <p>
+          <small>
+            Or Please the following form and I will get back to you <br />
+            as soon as possible
+          </small>
+        </p>
+      </div>
       <form>
         <div className="form-row">
           <div className="form-group col-md-6">
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,10 +1,26 @@
 import React from "react";
 import Header from "../components/Header";
+import pos from "../assets/screenshots/ph-pos-1.jpg";
 
 function Work() {
   return (
     <div className="work">
       <Header h1="Recent" span="Projects" />
+
+      <div className="work-list">
+        <div className="work-content">
+          <div className="work-content-img">
+            <img src={pos} alt="" />
+          </div>
+          <div className="work-content-text">
+            <h5>Pharmacy Management System</h5>
+          </div>
+        </div>
+
+        <div className="work-content">
+          <div className="work-content-placeholder">Others comming soon</div>
+        </div>
+      </div>
     </div>
   );
 }
